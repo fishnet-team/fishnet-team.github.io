7847f15fea2cac6069f39e9d36235f623cc7a803
Andrew Ishutin <hazmozavr@gmail.com>
2019-12-12 13:23:49 +0300

    Serverside livechat integration

--- server/server.py
+++ server/server.py
@@ -15,5 +15,13 @@ def log_all_webhooks():
     print(4, request.json)
     return {"result": "ok"}
 
+@app.route('/livechat', methods=['POST', 'GET'])
+def log_all_webhooks():
+    print(1, request.form)
+    print(2, request.args)
+    print(3, request.values)
+    print(4, request.json)
+    return {"result": "ok"}
+
 if __name__ == '__main__':
     app.run(debug=True, port=80, host="0.0.0.0") #run app in debug mode on port 5000
